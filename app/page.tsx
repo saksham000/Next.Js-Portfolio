6302f1dd4403c8b13000eb6e86bb52eac967b52e
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-10-07 21:48:53 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex justify-center mt-64 font-semibold text-3xl text-red-300">
+        <h1>Comming Soon !</h1>
+      </div>
+    </main>
+  );
+}
